134594c58a1b30391ffd82897125db954bf3bda3
PurpleStripedUnicorn <dirck.code@gmail.com>
2018-07-28 21:29:18 +0200

    project start

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# raspberry-pi monitor tool
+# Raspberry Pi Monitoring Tool
+
+With this tool you can easily monitor many of your raspberry pi's parts. Please
+read the following document before using the software on your website or server.
+
+## How it works
+You add the [server-monitor folder](../master/server-monitor/) folder to the root directory of
+your website. Now go to http://your-website.com/server-monitor and see the results.
+(You do not need a domain name, an IP address is enough)
+
+## Requirements
+The following things are or may be required for the monitoring tool to work properly:
+* A raspberry pi 3 Model B (not tested on other devices)
+* Apache installed on your raspberry pi
+* PHP ready on your apache server
+* All scripts have permission to use the "exec()" function in PHP (setting the folder's
+permission to 777 is enough most of the time)
+* Have "sysstat" installed on your raspberry pi
+
+## Troubleshooting
+It may be possible that you are experiencing bugs or other abnormalities. Please
+make sure you have read the list included above and have confirmed all the
+criteria are met, before reporting any bugs.
+
+## Other info
+### Programming languages
+The following programming languages and libraries were used for this tool:
+* HTML
+* JavaScript & jQuery
+* CSS
+* PHP
+* Linux shell
---
+++ server-monitor/get.php
@@ -0,0 +1,92 @@
+<?php
+
+$result = array();
+
+$start_time = microtime(true);
+
+
+
+### CPU USAGE ###
+# one command to get all usage
+exec(
+    "mpstat 1 1 -P ALL | awk '$12 ~ /[0-9.]+/ { print 100 - $12 }'",
+    $cpu_usage
+);
+# all cores combined
+$result["cpu_usage"] = number_format( $cpu_usage[0], 2, ".", "," );
+# core 0
+$result["cpu_usage_core0"] = number_format( $cpu_usage[1], 2, ".", "," );
+# core 1
+$result["cpu_usage_core1"] = number_format( $cpu_usage[2], 2, ".", "," );
+# core 2
+$result["cpu_usage_core2"] = number_format( $cpu_usage[3], 2, ".", "," );
+# core 3
+$result["cpu_usage_core3"] = number_format( $cpu_usage[4], 2, ".", "," );
+
+
+
+### CPU TEMPERATURE ###
+exec(
+    "cat /sys/class/thermal/thermal_zone0/temp",
+    $cpu_temp
+);
+$cpu_temp = number_format((int)$cpu_temp[0] * 0.001, 1, ".", ",");
+$result["cpu_temp"] = $cpu_temp . "°C";
+
+
+
+### TOTAL DISK SPACE ###
+exec(
+    "df -h | awk '$2 { print $2 }' | sed -n '2 p'",
+    $total_disk_space
+);
+$result["storage_total"] = $total_disk_space[0]."iB";
+
+
+
+### FREE DISK SPACE ###
+exec(
+    "df -h | awk '$4 { print $4 }' | sed -n '2 p'",
+    $free_disk_space
+);
+$result["storage_free"] = $free_disk_space[0]."iB";
+
+
+
+### USED DISK SPACE ###
+exec(
+    "df -h | awk '$3 { print $3 }' | sed -n '2 p'",
+    $used_disk_space
+);
+$result["storage_used"] = $used_disk_space[0]."iB";
+
+
+
+### RAM USAGE/DISTRIBUTION ###
+# one command for all info
+exec(
+    ("free -m | awk '$2 { print $2 }' | sed -n '2 p' && ".
+     "free -m | awk '$3 { print $3 }' | sed -n '2 p' && ".
+     "free -m | awk '$4 { print $4 }' | sed -n '2 p' && ".
+     "free -m | awk '$5 { print $5 }' | sed -n '2 p' && ".
+     "free -m | awk '$6 { print $6 }' | sed -n '2 p' && ".
+     "free -m | awk '$7 { print $7 }' | sed -n '1 p'"),
+    $ram_usage
+);
+$result["ram_total"] = $ram_usage[0]."M";
+$result["ram_used"] = $ram_usage[1]."M";
+$result["ram_free"] = $ram_usage[2]."M";
+$result["ram_shared"] = $ram_usage[3]."M";
+$result["ram_buff_cache"] = $ram_usage[4]."M";
+$result["ram_available"] = $ram_usage[5]."M";
+
+
+
+### TIME THIS SCRIPT TOOK ###
+# time in microseconds
+$result["query_time"] = number_format( (microtime(true) - $start_time), 3, ".", "," )."s";
+
+
+
+# get the first result
+echo json_encode($result);
