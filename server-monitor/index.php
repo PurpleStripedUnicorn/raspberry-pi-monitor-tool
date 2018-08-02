a247fe8c140b4e36bb7e35bc9ab3eb92522469ee
PurpleStripedUnicorn <dirck.code@gmail.com>
2018-08-02 22:15:00 +0200

    better responsive web design

--- server-monitor/index.php
+++ server-monitor/index.php
@@ -12,6 +12,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <!-- main styling -->
     <link rel="stylesheet" type="text/css" href="server-monitor.css" />
+    <meta name="viewport" content="width=screen-width, initial-scale=1.0" />
 
 </head>
 <body>
