06324a7c780e5d718674522879a3919dee89ec0d
PurpleStripedUnicorn <dirck.code@gmail.com>
2018-08-27 19:54:59 +0200

    updated head message

--- server-monitor/head_message.php
+++ server-monitor/head_message.php
@@ -1,3 +1,3 @@
 <?php include __DIR__."/init.php"; ?>
 <!-- Made by PurpleStripedUnicorn
-     See license details on GitHub -->
+     See license details on GitHub (https://github.com/PurpleStripedUnicorn/raspberry-pi-monitor-tool/blob/master/LICENSE) -->
